0d2fe8da6f4cae2ee435e6a5a3d80282b1f41c74
Mike <mclancy96@gmail.com>
2025-08-15 14:53:56 -0400

    Build out class controller and copy over views

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,36 @@
 class SchoolClassesController < ApplicationController
+  before_action :set_school_class, only: %i[show edit update]
+
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show; end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.create!(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+  end
+
+  def update
+    @school_class.update!(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def set_school_class
+    @school_class = SchoolClass.find(params[:id])
+  end
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
 end
